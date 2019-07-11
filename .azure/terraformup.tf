192b031db8aafeb5ef10ad3c1cb00ed2e3fc2b38
fred belotte <fredbelotte@outlook.com>
2019-07-11 10:37:43 -0500

    add terraform configuration file [#167227067]

--- .azure/terraformup.tf
+++ .azure/terraformup.tf
@@ -1,5 +1,36 @@
 # housingxyz :: terraform
 
-module "az_housingxyz" {
-  source = "https://github.com/revaturexyz/project-alpine/blob/master/src/az/startup.tf"
+## providers
+provider "azuread" {
+  version = "~>0.4.0"
+}
+
+provider "azurerm" {
+  version = "~>1.30.0"
+}
+
+provider "aws" {
+  version = "~>2.14.0"
+}
+
+provider "google" {
+  version = "~>2.8.0"
+}
+
+provider "random" {
+  version = "~>2.1.0"
+}
+
+## variables
+variable "az_locations" {
+  type = "map"
+  default = {
+    main = "southcentralus"
+  }
+}
+
+## resource groups
+resource "azurerm_resource_group" "az_housingxyz" {
+  name = "housingxyz-group"
+  location = "${var.az_locations["main"]}"
 }
