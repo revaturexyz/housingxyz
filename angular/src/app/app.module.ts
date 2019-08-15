fde24cbb67a09558802943f2c68715f68a1be2fa
Fan Chen <chenfan0213@gmail.com>
2019-08-15 16:06:29 -0400

    merging to broker

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Room } from 'src/Entities/room';
+import { Provider } from 'src/Entities/provider';
+import { ProviderLocation } from 'src/Entities/location';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AddRoomComponent } from './add-room/add-room.component';
+import { DeleteRoomComponent } from './delete-room/delete-room.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+import { ApiService } from './api.service';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { StickyNavModule } from 'ng2-sticky-nav';
+import { MsAdalAngular6Module,AuthenticationGuard } from 'microsoft-adal-angular6';
+import { environment } from '../environments/environment';
+import { AddLocationComponent } from './add-location/add-location.component';
+import { ShowByLocationComponent } from './show-by-location/show-by-location.component';
+import { DatePipe } from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    AddRoomComponent,
+    DeleteRoomComponent,
+    UpdateRoomComponent,
+    AddLocationComponent,
+    ShowByLocationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),//withConfig: remove warning message when using formcontrolname and ngModel
+    StickyNavModule,
+    MsAdalAngular6Module.forRoot({
+      tenant: environment.tenant,
+      clientId: environment.clientId,
+      redirectUri: window.location.origin,
+      endpoints: environment.endpoints,
+      navigateToLoginRequestUrl: false,
+      extraQueryParameter: environment.extraQueryParameter,
+      cacheLocation: 'localStorage'
+    })
   ],
-  providers: [],
+  providers: [Room, Provider, ProviderLocation, ApiService, AuthenticationGuard, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular/src/app/home/home.component.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
+import { MsAdalAngular6ServiceMock } from '../testing/mock/Mock-adal-service';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeComponent ],
+      imports:[
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers:[
+        {
+          provide:MsAdalAngular6Service,useClass:MsAdalAngular6ServiceMock
+        }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+});
