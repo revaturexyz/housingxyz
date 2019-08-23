fc5d9b78501685dffb6bf2587e6e644e882bb3bc
fred belotte <fredbelotte@outlook.com>
2019-08-23 00:24:34 -0500

    add dev, pre, stg angular environments [#167390494]

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,8 +1,3 @@
 export const environment = {
-  msalConfig: {
-    authority: 'https://identityxyz.b2clogin.com/7cd0af2d-1ead-4ab0-bbbc-9913da89ce5a/B2C_1_signup_signin',
-    clientID: '398b1b9c-8b43-4f29-bac5-b4b0daf30e04',
-    validateAuthority: false
-  },
   production: true
 };
---
+++ angular/src/environments/environment.stg.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  msalConfig: {
+    authority: 'https://identityxyz.b2clogin.com/7cd0af2d-1ead-4ab0-bbbc-9913da89ce5a/B2C_1_signup_signin',
+    clientID: '398b1b9c-8b43-4f29-bac5-b4b0daf30e04',
+    redirectUri: 'https://housingstg.revature.xyz',
+    validateAuthority: false
+  },
+  production: false
+};
