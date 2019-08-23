9cbc0d1b6fa8eca6c1763158e61b2198f896fe51
fred belotte <fredbelotte@outlook.com>
2019-08-23 00:53:18 -0500

    fix missing reference in app component test [#167390494]

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -1,16 +1,22 @@
-import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+
+import { MsalService } from '@azure/msal-angular';
+import { TestBed, async } from '@angular/core/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        MsalService
+      ]
     }).compileComponents();
   }));
 
