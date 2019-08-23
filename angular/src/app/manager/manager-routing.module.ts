2dce7fa5b730667e1fd5d51550cc90992ec52e74
fred belotte <fredbelotte@outlook.com>
2019-08-22 23:35:10 -0500

    add manager component with routing support [#167390494]

--- angular/src/app/manager/manager-routing.module.ts
+++ angular/src/app/manager/manager-routing.module.ts
@@ -1,8 +1,12 @@
+import { ManagerComponent } from './manager.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MsalGuard } from '@azure/msal-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'manager', component: ManagerComponent, canActivate: [MsalGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- angular/src/app/manager/manager.module.ts
+++ angular/src/app/manager/manager.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ManagerRoutingModule } from './manager-routing.module';
+import { NgModule } from '@angular/core';
 
+import { ManagerComponent } from './manager.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ManagerComponent],
   imports: [
     CommonModule,
     ManagerRoutingModule
