21cf92494126fa582e78b46accef5cc8a57cbff3
bladengar <bladengar@gmail.com>
2019-09-05 10:52:19 -0500

    fixed complex constructor

--- angular/src/models/complex.ts
+++ angular/src/models/complex.ts
@@ -6,14 +6,13 @@ export class Complex {
     ZipCode: string;
     ComplexName: string;
     ContactNumber: string;
-    constructor(
-        ComplexId: number,
-        StreetAddress: string,
-        City: string,
-        State: string,
-        ZipCode: string,
-        ComplexName: string,
-        ContactNumber: string
-    )
-    {}
+    constructor(id: number, addr: string, city: string, state: string, zip: string, name: string, num: string){
+        this.ComplexId = id;
+        this.StreetAddress = addr;
+        this.City = city;
+        this.State = state;
+        this.ZipCode = zip;
+        this.ComplexName = name;
+        this.ContactNumber = num;
+    }
 }
