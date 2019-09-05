1276ff04f48a0cb6703586dcf9691ef19797f42e
Alex <alexyandukin@gmail.com>
2019-09-05 12:31:53 -0500

    Added Something

--- angular/src/models/amenity.ts
+++ angular/src/models/amenity.ts
@@ -1,9 +1,4 @@
 export class Amenity {
     AmenityId: number;
     Amenity: string;
-
-    constructor(id: number, amenity: string) {
-        this.AmenityId = id;
-        this.Amenity = amenity;
-    }
 }
