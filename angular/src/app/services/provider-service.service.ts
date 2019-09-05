59f3eb3cab13f2641a80492138dbebd253ec6bcb
bladengar <bladengar@gmail.com>
2019-09-05 09:32:49 -0500

    added httpclient to provider service

--- angular/src/app/services/provider-service.service.ts
+++ angular/src/app/services/provider-service.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Provider } from '../../models/Provider';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProviderServiceService {
 
-  constructor() { }
+  constructor(private httpBus: HttpClient) { }
+
+  
 }
