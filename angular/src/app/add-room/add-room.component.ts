9f52da18a1899f7e79d383578748092c0a233c83
Angel Iniguez <angel.inigue1996@gmail.com>
2019-09-05 14:11:04 -0500

    created the add room component and I also added the routing so that the user is directed to the correct page.

---
+++ angular/src/app/add-room/add-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dev-add-room',
+  templateUrl: './add-room.component.html',
+  styleUrls: ['./add-room.component.scss']
+})
+export class AddRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
