9f4c99b264cf29b55c4cf92f8274ea838806efb3
Angel Iniguez <angel.inigue1996@gmail.com>
2019-09-06 16:39:17 -0500

    created the trainingcenter service

--- angular/src/app/services/trainingcenter.service.ts
+++ angular/src/app/services/trainingcenter.service.ts
@@ -2,16 +2,40 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Room } from '../../models/room';
 import { Amenity} from '../../models/amenity';
-import { Observable } from 'rxjs';
+import { Trainingcenter} from '../../models/trainingcenter';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingcenterService {
 
+  trainingcenter: Trainingcenter = new Trainingcenter(
+    1,
+    "UTA",
+    "1001 s. Center St",
+    "Arlington",
+    "Texas",
+    "76010",
+    "UTA",
+    "1231231234"
+  );
+  trainingcenter2: Trainingcenter = new Trainingcenter(
+    2,
+    "UIC",
+    "123 s. Chicago Ave",
+    "Chicago",
+    "Illinois",
+    "60645",
+    "UIC",
+    "3213213214"
+  );
   constructor(private http : HttpClient) { }
 
-  // getTrainingCenters(): Observable<TrainingCenter[]>
-  // get full details (ie. address information)
-  // getComplexesByTrainingCenter(centerId: int): Observable<Complex[]>
+  getTrainingCenters(): Observable<Trainingcenter[]>{
+    return of([this.trainingcenter, this.trainingcenter2]);
+  }
+  getComplexesByTrainingCenter(centerId: number): Observable<Trainingcenter[]>{
+    return of([this.trainingcenter, this.trainingcenter2]);
+  }
 }
