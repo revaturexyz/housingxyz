fb90063b194842f999b75806b2b773e9484985cc
Elijah Arnold <elijah.m.arnold@gmail.com>
2019-09-06 16:33:46 -0500

    updated add-room component with select options for choosing which living complex a room should be associated with

--- angular/src/app/add-room/add-room.component.ts
+++ angular/src/app/add-room/add-room.component.ts
@@ -5,6 +5,7 @@ import { ShowByLocationComponent } from '../show-by-location/show-by-location.co
 import { Complex } from 'src/models/complex';
 import { Observer } from 'rxjs';
 import { ProviderServiceService } from '../services/provider-service.service';
+import { Address } from 'src/models/address';
 
 @Component({
   selector: 'dev-add-room',
@@ -14,10 +15,15 @@ import { ProviderServiceService } from '../services/provider-service.service';
 export class AddRoomComponent implements OnInit {
   room: Room;
   show: boolean = false;
+
   complexList: Complex[];
   activeComplex: Complex;
   complexShowString: string = "Choose Complex";
 
+  addressList: Address[];
+  activeAddress: Address;
+  addressShowString: string = "Choose Address";
+  
   constructor(
     private roomService: RoomService,
     private providerService: ProviderServiceService
@@ -43,14 +49,23 @@ export class AddRoomComponent implements OnInit {
   }
   ngOnInit() {
     this.providerService.getComplexes(1).subscribe(this.complexObs);
+    this.providerService.getAddressesByProvider(1).subscribe(this.addressObs);
     console.log(this.roomService.getRoomTypes());
     console.log(this.roomService.getRoomsByProvider(1));
   }
 
-  // An observable for receiving and prcessing return values
+  // An Observer for receiving and prcessing return values
   // from the providerService.getComplexes() method
   complexObs: Observer<Complex[]> = {
     next: x => {console.log('Observer got a next value.'); this.complexList = x},
+    error: err => console.error('Observer got an error: ' + err),
+    complete: () => console.log('Observer got a complete notification'),
+  };
+
+  // An Observer for receiving and processing return values
+  // from the providerService.getAddressesByProvider() method
+  addressObs: Observer<Address[]> = {
+    next: x => {console.log('Observer got next value.'); this.addressList = x},
     error: err => console.error('Observer got an error: ' + err),
     complete: () => console.log('Observer got a complete notification'),
   };
@@ -64,9 +79,16 @@ export class AddRoomComponent implements OnInit {
   }
 
   // Updates selected complex property and display string
-  // based on what is selected.
+  // based on what is selected
   complexChoose(complex: Complex) {
     this.complexShowString = complex.ComplexName + ' | ' + complex.ContactNumber ;
     this.activeComplex = complex;
+  }
+
+  // Updates selected address property and display string
+  // based on what is selected
+  addressChoose(address: Address) {
+    this.addressShowString = address.StreetAddress;
+    this.activeAddress = address;
   }
 }
