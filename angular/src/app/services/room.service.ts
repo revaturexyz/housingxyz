b7f6a72b46fb1a8b6fd8fdecc52d6b332b6443aa
Angel Iniguez <angel.inigue1996@gmail.com>
2019-09-06 11:39:33 -0500

    created a shared branch

--- angular/src/app/services/room.service.ts
+++ angular/src/app/services/room.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Room } from '../../models/room';
-import { Amenity} from '../../models/amenity';
+import { Amenity } from '../../models/amenity';
 import { Observable, of, from } from 'rxjs';
 import { Complex } from 'src/models/complex';
 import { Address } from 'src/models/address';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class RoomService {
     dummyGender: string[] = ['male', 'female', 'undefined'];
@@ -15,16 +15,16 @@ export class RoomService {
     dummyAmenity2: Amenity = new Amenity(2, 'smart TV');
     dummmyList: Amenity[] = [this.dummyAmenity1, this.dummyAmenity2];
     room: Room = new Room(null, new Address(1, '1001 S Center St', 'Arlington', 'TX', '76010'),
-                            '2210', 2, 'Apartment', false, new Amenity(1, 'Patio'), new Date(), new Date(), 1);
+        '2210', 2, 'Apartment', false, new Amenity(1, 'Patio'), new Date(), new Date(), 1);
     room2: Room = new Room(null, new Address(2, '701 S Nedderman Dr', 'Arlington', 'TX', '76019'),
-                            '323', 9001, 'Dorm', true, new Amenity(2, 'Washer/Dryer'), new Date(), new Date(), 2);
+        '323', 9001, 'Dorm', true, new Amenity(2, 'Washer/Dryer'), new Date(), new Date(), 2);
     constructor(private http: HttpClient) { }
     getRoomById(id: number): Observable<Room> {
         return of(this.room);
     }
     postRoom(obj: Room): Observable<Room> {
         return of(new Room(obj.RoomID, obj.Address, obj.RoomNumber, obj.NumberOfBeds,
-                            obj.RoomType, obj.IsOccupied, obj.Amenities, obj.StartDate, obj.EndDate, obj.ComplexID));
+            obj.RoomType, obj.IsOccupied, obj.Amenities, obj.StartDate, obj.EndDate, obj.ComplexID));
     }
     getRoomsByProvider(providerId: number): Observable<Room[]> {
         return of([this.room, this.room2]);
@@ -37,14 +37,14 @@ export class RoomService {
             const GenderList: string[] = this.dummyGender;
             sub.next(GenderList);
             sub.complete();
-          });
+        });
         return simpleObservable;
     }
     getAmenities(): Observable<Amenity[]> {
         const simpleObservable = new Observable<Amenity[]>((sub) => {
             const GenderList: Amenity[] = this.dummmyList;
             sub.complete();
-          });
+        });
         return simpleObservable;
     }
 }
