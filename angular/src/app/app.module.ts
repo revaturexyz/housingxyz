ba5a857e24d5459088b3bfbac5018419056b47c6
Elijah Arnold <elijah.m.arnold@gmail.com>
2019-09-07 10:06:07 -0500

    ported over update-room component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,31 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Room } from 'src/models/room';
 import { Provider } from 'src/models/provider';
 import { ProviderLocation } from 'src/models/location';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 import { StickyNavModule } from 'ng2-sticky-nav';
-import { AuthenticationGuard } from 'microsoft-adal-angular6';
+import { MsAdalAngular6Module,AuthenticationGuard } from 'microsoft-adal-angular6';
+import { environment } from '../environments/environment';
+import { AddLocationComponent } from './add-location/add-location.component';
+import { ShowByLocationComponent } from './show-by-location/show-by-location.component';
+import { DatePipe, CommonModule } from '@angular/common';
+import { RouterLinkDirectiveStub } from './testing/router-link-directive-stub';
 import { AddRoomComponent } from './add-room/add-room.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
     LoginComponent,
-    AddRoomComponent
+    AddLocationComponent,
+    ShowByLocationComponent,
+    RouterLinkDirectiveStub,
+    AddRoomComponent,
+    UpdateRoomComponent,
+    RoomDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    NgbModule,
     ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),//withConfig: remove warning message when using formcontrolname and ngModel
     StickyNavModule
   ],
---
+++ angular/src/app/update-room/update-room.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Complex } from 'src/models/complex';
+import { ProviderServiceService } from '../services/provider-service.service';
+import { Observer } from 'rxjs';
+import { Room } from 'src/models/room';
+import { RoomService } from '../services/room.service';
+
+@Component({
+  selector: 'dev-update-room',
+  templateUrl: './update-room.component.html',
+  styleUrls: ['./update-room.component.scss']
+})
+export class UpdateRoomComponent implements OnInit {
+
+  complexList: Complex[];
+  activeComplex: Complex;
+  roomList: Room[];
+  complexRooms: Room[];
+
+  complexObs: Observer<Complex[]> = {
+    next: x => {console.log('Observer got a next value.'); this.complexList = x},
+    error: err => console.error('Observer got an error: ' + err),
+    complete: () => console.log('Observer got a complete notification'),
+  };
+
+  roomsObs: Observer<Room[]> = {
+    next: x => {console.log('Observer got a next value.'); this.roomList = x},
+    error: err => console.error('Observer got an error: ' + err),
+    complete: () => console.log('Observer got a complete notification'),
+  };
+
+  showString: string = 'Choose Complex';
+
+  constructor(private providerService: ProviderServiceService, private roomService: RoomService) { }
+
+  ngOnInit() {
+    this.providerService.getComplexes(1).subscribe(this.complexObs);
+    this.roomService.getRoomsByProvider(1).subscribe(this.roomsObs);
+  }
+
+  complexChoose(complex: Complex) {
+    this.showString = complex.ComplexName;
+    this.activeComplex = complex;
+    // console.log(this.roomList);
+    this.complexRooms = this.roomList.filter(r => r.ComplexID == this.activeComplex.ComplexId);
+  }
+}
