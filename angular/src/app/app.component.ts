4e8df97c448049a147073ecc0ab2de255989fec0
Elijah Arnold <elijah.m.arnold@gmail.com>
2019-09-08 12:02:05 -0500

    removed auth code for now to make a comeback in a different feature branch

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -9,48 +9,4 @@ import * as Msal from 'msal';
 })
 export class AppComponent {
   title = 'housingxyz';
-  //   config = {
-  //     auth: {
-  //         clientId: "",
-  //         redirectUri: "https://localhost:4200",
-  //     },
-
-  // }
-
-  //   applicationId = ""; // B2C application Id
-  //   tenant = ""; // Azure tenant ID
-  //   signUpSignInPolicy = "B2C_1_revapp"; // Name of user flow
-
-  //   // name of scope, taken from the portal
-  //   scope = [""];
-
-  //   // the creation of this was taken from the ref above.
-  //   authority = "https://login.microsoftonline.com/tfp/" + this.tenant + "/" +
-  //     this.signUpSignInPolicy;
-
-  //   clientApplication = new Msal.UserAgentApplication(this.config);
-
-  //   login() {
-
-  //     var _this = this; // JS this :(
-
-  //     let loginRequest = {
-  //       scopes: ["user.read", "user.write"],
-  //       prompt: "select_account",
-  //   }
-
-  //   let accessTokenRequest = {
-  //       scopes: ["user.read", "user.write"]
-  //   }
-
-  //   _this.clientApplication.loginPopup(loginRequest).then(function (loginResponse) {
-  //       return _this.clientApplication.acquireTokenSilent(accessTokenRequest);
-  //   }).then(function (accessTokenResponse) {
-  //       const token = accessTokenResponse.accessToken;
-  //       console.log(token)
-  //   }).catch(function (error) {
-  //       //handle error
-  //   });
-
-  //   }
 }
