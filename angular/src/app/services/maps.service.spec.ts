1780c9bdf3766132e10d2a4975903d486a9d4a9f
Elijah Arnold <elijah.m.arnold@gmail.com>
2019-09-10 15:55:19 -0500

    fixed default maps service test

--- angular/src/app/services/maps.service.spec.ts
+++ angular/src/app/services/maps.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MapsService } from './maps.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MapsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  }));
 
   it('should be created', () => {
     const service: MapsService = TestBed.get(MapsService);
