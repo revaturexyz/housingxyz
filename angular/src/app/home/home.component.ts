18515acce4362016ce03cf9a4f811ed63910aab1
Alex <alexyandukin@gmail.com>
2019-09-12 11:37:16 -0500

    reverted home component back to the way it was

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ɵConsole } from '@angular/core';
 import { Router } from '@angular/router';
-import { MapsService } from '../services/maps.service';
 
 @Component({
   selector: 'dev-home',
@@ -11,13 +10,11 @@ export class HomeComponent implements OnInit {
   locationList: object;
   roomList: object;
 
-  constructor(private router: Router, private mapsService: MapsService) { }
+  constructor(private router: Router) { }
 
   updateRoom(id: number) {
     this.router.navigate(['update-room', id]);
   }
 
-  ngOnInit() {
-    this.mapsService.checkDistance();
-  }
+  ngOnInit() { }
 }
