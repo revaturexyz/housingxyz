b89a188cec6e15a5a69b3685a8559b2aa2999aab
Elijah Arnold <elijah.m.arnold@gmail.com>
2019-09-18 09:57:03 -0500

    added providerXYZ endpoint to environment.prod.ts

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   extraQueryParameter: 'nux=1', // This is optional
   googleMapsKey: 'AIzaSyCxYMcmEjlHQ2r2CywMgyK7YEplxurqW2A',
   endpoints: {
+    providerXYZ: 'https://provider.revature.xyz/', // Note, this is an object, you could add several things here
     'http://localhost:4200': '' // Note, this is an object, you could add several things here
   }
 };
