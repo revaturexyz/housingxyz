c91ba7b855b2cd40488f395f523c2526527acd8b
Elijah Arnold <elijah.m.arnold@gmail.com>
2019-09-17 21:26:16 -0500

    fixed default test for new service and login component

--- angular/src/app/services/redirect.service.spec.ts
+++ angular/src/app/services/redirect.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RedirectService } from './redirect.service';
 
 describe('RedirectService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ RouterTestingModule ]
+  }));
 
   it('should be created', () => {
     const service: RedirectService = TestBed.get(RedirectService);
