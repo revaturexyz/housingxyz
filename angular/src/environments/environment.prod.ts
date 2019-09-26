713791945ac867bd7f28874246bc8b1b6d972f26
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-09-26 15:39:12 -0500

    clean up environment config a bit.

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,16 +1,10 @@
 export const environment = {
   production: true,
-  tenant: '',
-  clientId: '',
-  extraQueryParameter: 'nux=1', // This is optional
+  authority: 'https://identityxyz.b2clogin.com/identity.revature.xyz/b2c_1_signup_signin',
+  clientID: 'fac95bb7-bd11-4ecf-b30b-fdd6ddd13bcd',
+  validateAuthority: false,
   googleMapsKey: 'AIzaSyCxYMcmEjlHQ2r2CywMgyK7YEplxurqW2A',
-  identity: {
-    authority: 'https://identityxyz.b2clogin.com/identity.revature.xyz/b2c_1_signup_signin',
-    clientID: 'fac95bb7-bd11-4ecf-b30b-fdd6ddd13bcd',
-    validateAuthority: false
-  },
   endpoints: {
-    providerXYZ: 'https://providerpre.revature.xyz/',
-    'http://localhost:4200': '' // Note, this is an object, you could add several things here
+    providerXYZ: 'https://providerpre.revature.xyz/'
   }
 };
