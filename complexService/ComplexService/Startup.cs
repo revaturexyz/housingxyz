4220fec5891cf03d5743c178ac2efdf310852d8b
Hsuan-Yu Lin <o100249243@gmail.com>
2019-11-14 12:25:25 -0600

    api, logic, database initialized

--- complexService/ComplexService/Startup.cs
+++ complexService/ComplexService/Startup.cs
@@ -25,7 +25,34 @@ namespace ComplexService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy(MyAllowSpecificOrigins,
+            //    builder =>
+            //    {
+            //        builder.WithOrigins("http://localhost:4200",
+            //                            "https://sweentened-angular.azurewebsites.net")
+            //                                //Might need to add the app service website from microsoft azure site here in WithOrigins
+
+            //                                //Added this to allow any method or header in angular to prevent errors
+            //                                .AllowAnyHeader()
+            //                                .AllowAnyMethod()
+            //                                .AllowCredentials();
+            //    });
+            //});
+
+
+            string connectionString = Configuration.GetConnectionString("complex");
+
+            // among the services you register for DI (dependency injection)
+            // should be your DbContext.
+            //services.AddDbContext<SWTDbContext>(options =>
+            //{
+            //    options.UseNpgsql(connectionString);
+            //});
+            //services.AddControllers();
+
+            //services.AddScoped<IRepo, Repo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ complexService/ComplexServiceDatabase/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ComplexServiceDatabase
+{
+    public class Class1
+    {
+    }
+}
