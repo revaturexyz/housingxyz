2b2cdfa0cbb9fc0b2d76aa23d9cf1ced4550511a
Joe Mohrbacher <jhmohrbacher@gmail.com>
2019-11-14 11:00:42 -0600

    added initial setup of tenant model and controler

---
+++ tenant/aspnet/Xyz.Tenant.Api/Controllers/TenantsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Xyz.Tenant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenantsController : ControllerBase
+    {
+        // GET: api/Tenants
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Tenants/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Tenants
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Tenants/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ tenant/aspnet/Xyz.Tenant.Lib/Models/Tenant.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xyz.Tenant.Lib.Models
+{
+  class Tenant
+  {
+    private int _id;
+    private string _firstName;
+    private string _lastName;
+    private string _email;
+    private int _addressId;
+    private int _roomId;
+    private int _carId;
+
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public int AddressId { get; set; }
+    public int RoomId { get; set; }
+    public int CarId { get; set; }
+  }
+}
