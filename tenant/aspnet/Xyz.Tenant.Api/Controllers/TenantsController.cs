bfe0991f9d1d8651e4ea5b35e0094321ff2d2a98
Colton Clary <coltonclary0806@yahoo.com>
2019-11-14 14:05:58 -0600

    added colton branch and added GetTenantByIdAsync

--- tenant/aspnet/Xyz.Tenant.Api/Controllers/TenantsController.cs
+++ tenant/aspnet/Xyz.Tenant.Api/Controllers/TenantsController.cs
@@ -1,43 +1,77 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Xyz.Tenant.Lib.Interface;
+using Xyz.Tenant.Api;
 
 namespace Xyz.Tenant.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TenantsController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TenantsController : ControllerBase
+  {
+
+    //private readonly IDataAccess db;
+
+    //public TenantsController(IDataAccess dataAccess)
+    //{
+    //  db = dataAccess;
+    //}
+    private readonly ITenantRepository _tenantRepository;
+
+    public TenantsController(ITenantRepository tenantRepository)
     {
-        
-        //private readonly IDataAccess db;
+      _tenantRepository = tenantRepository ?? throw new ArgumentNullException(nameof(tenantRepository), "Tenant Cannot be null");
+    }
 
-        //public TenantsController(IDataAccess dataAccess)
-        //{
-        //  db = dataAccess;
-        //}
-
-
+    /// <summary>
+    /// Get all tenants
+    /// </summary>
+    /// <returns></returns>
         // GET: api/Tenants
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+    /// <summary>
+    /// Get Tenant by Id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    // GET: api/Tenants/5
+    //api/[controller]
+    [HttpGet("{id}", Name = "Get")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Xyz.Tenant.Api.Models.Tenant>> GetTenantByIdAsync([FromRoute] int id)
+    {
+      try
+      {
+        var tenant = await _tenantRepository.GetTenantByIdAsync(id);//this is a repository function that should be async and return Task<IEnumerable<Xyz.Tenant.Lib.Models.Tenant>>
+                                                                        //given an int Id
+        return Ok(tenant);
 
-        // GET: api/Tenants/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+      }
+      catch(ArgumentException)
+      {
+        return NotFound();
+      }
+      catch(Exception e)
+      {
+        return StatusCode(500, e.Message);
+      }
 
-        // POST: api/Tenants
-        [HttpPost]
+    }
+
+    // POST: api/Tenants
+    [HttpPost]
         public void Post([FromBody] string value)
         {
+          
         }
 
         // PUT: api/Tenants/5
