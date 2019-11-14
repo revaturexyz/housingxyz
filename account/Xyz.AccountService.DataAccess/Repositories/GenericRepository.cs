8e8aaa9cf2ec774fad2b8442eae518bde89db07c
PhatNguyen203 <Lee0ne203@gmail.com>
2019-11-14 16:28:42 -0600

    init interfaces and setup connection string with postgresql

---
+++ account/Xyz.AccountService.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xyz.AccountService.DataAccess.Repositories
+{
+	class GenericRepository
+	{
+	}
+}
---
+++ account/Xyz.AccountService.Lib/Interface/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xyz.AccountService.Lib.Interface
+{
+	public interface IGenericRepository : IAsyncDisposable
+	{
+	}
+}
