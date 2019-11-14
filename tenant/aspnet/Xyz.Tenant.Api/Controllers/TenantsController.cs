50efcb0a1fdaad5fea2b4f9ec9df3ab52f083d88
Colton Clary <coltonclary0806@yahoo.com>
2019-11-14 11:39:51 -0600

    Added Constructor in TenantsController and Tenant Interface

--- tenant/aspnet/Xyz.Tenant.Api/Controllers/TenantsController.cs
+++ tenant/aspnet/Xyz.Tenant.Api/Controllers/TenantsController.cs
@@ -1,46 +1,68 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Xyz.Tenant.Lib.Interface;
 
 namespace Xyz.Tenant.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TenantsController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TenantsController : ControllerBase
+  {
+
+    private readonly ITenantRepository _tenantRepository;
+
+    public TenantsController(ITenantRepository tenantRepository)
     {
-        // GET: api/Tenants
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Tenants/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Tenants
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Tenants/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      _tenantRepository = tenantRepository ?? throw new ArgumentNullException(nameof(tenantRepository), "Tenant repo cannot be null");
     }
+
+    // GET: api/Tenants
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    /// <summary>
+    /// Get all tenants by tenantID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    // GET: api/Tenants/5
+    [HttpGet("{id}", Name = "Get")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public string Get(int id)
+    {
+      //try
+      //{
+      //  var tenant = await _tenantRepository.GetTenantsByIdAsync(tenantId);
+      //  var apiTenant = tenant.Select(t => new Tenants)
+      //}
+      return "value";
+    }
+
+    // POST: api/Tenants
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT: api/Tenants/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE: api/ApiWithActions/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
 }
---
+++ tenant/aspnet/Xyz.Tenant.Lib/Interface/ITenantRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xyz.Tenant.Lib.Interface
+{
+  public interface ITenantRepository
+  {
+  }
+}
