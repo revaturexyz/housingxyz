cf1ef73ef8baf48c2514855c7ebb1d3a57ea30f3
Mark Moore <mark.c.moore@hotmail.com>
2019-11-14 16:32:25 -0600

    add test suite and a test

---
+++ complexService/ComplexService/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplexServiceApi
+{
+    public class TestClass
+    {
+        public int TestTheTestSuit(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
