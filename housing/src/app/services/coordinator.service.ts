699c12732fbf6c7ac7a66655874028f01c3da531
Gregory Y Favrot <gpyofavv@gmail.com>
2019-11-15 15:15:42 -0600

    Remove TODO comments

--- housing/src/app/services/coordinator.service.ts
+++ housing/src/app/services/coordinator.service.ts
@@ -8,6 +8,5 @@ import { environment } from 'src/environments/environment';
 
 // Service for AJAX Calls to various Rest APIs needed by Coordinators
 export class CoordinatorService {
-  // add url from environment - need to add coordinator to endpoints
-  // Add http client
+
 }
