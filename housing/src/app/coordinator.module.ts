72a8048e794aed2039ffe09f1ba1dd59d02a1ef7
Gregory Y Favrot <gpyofavv@gmail.com>
2019-11-15 13:23:58 -0600

    Remove conflicting modules' imports to build prod

--- housing/src/app/coordinator.module.ts
+++ housing/src/app/coordinator.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RoomDetailsComponent } from './room-details/room-details.component';
-import { NavComponent } from './nav/nav.component';
 import { MsalModule, MsalInterceptor } from '@azure/msal-angular';
 
 
 @NgModule({
   declarations: [
-    NavComponent,
-    RoomDetailsComponent
   ],
   imports: [
     CommonModule,
