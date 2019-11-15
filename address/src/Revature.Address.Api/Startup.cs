be169321e70dc2baeeec80fc88aea238b5aae380
Girard Sanford <girard.sanford@gmail.com>
2019-11-15 16:22:12 -0600

    added rebuilt address service

---
+++ address/src/Revature.Address.Api/Startup.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Revature.Address.DataAccess.Entities;
+using Revature.Address.DataAccess.Interfaces;
+using Revature.Address.Lib.Interfaces;
+
+namespace Revature.Address.Api
+{
+  public class Startup
+  {
+    private const string ConnectionStringName = "AddressDb";
+    private const string CorsPolicyName = "RevatureCorsPolicy";
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddEntityFrameworkNpgsql().AddDbContext<AddressDbContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString(ConnectionStringName)));
+
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy(CorsPolicyName, builder =>
+        {
+          builder.WithOrigins("http://localhost:4200",
+                              "https://localhost:4200",
+                              "http://housing.revature.xyz",
+                              "https://housing.revature.xyz",
+                              "http://housingdev.revature.xyz",
+                              "https://housingdev.revature.xyz")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+        });
+      });
+
+      services.AddScoped<IMapper, DataAccess.Mapper>();
+      services.AddScoped<IDataAccess, DataAccess.DataAccess>();
+      services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Revature Address", Version = "v1" });
+      });
+
+      services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseSerilogRequestLogging();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Revature Address V1");
+      });
+
+      app.UseRouting();
+
+      app.UseCors(CorsPolicyName);
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
