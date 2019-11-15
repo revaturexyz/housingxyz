36b57345d8d4ce47693260c7db2db2b81a94f8b1
Gregory Y Favrot <gpyofavv@gmail.com>
2019-11-15 14:47:32 -0600

    Add describe to coordinator.service.spec.ts

--- housing/src/app/services/coordinator.service.spec.ts
+++ housing/src/app/services/coordinator.service.spec.ts
@@ -1,8 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { CoordinatorService } from './coordinator.service';
 
-it('should be created', () => {
-    const service: CoordinatorService = TestBed.get(CoordinatorService);
-    expect(service).toBeTruthy();
-  });
+describe('CoordinatorService', () => {
+    let myCoordinator: CoordinatorService;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+            ],
+            providers: [
+                CoordinatorService
+            ]
+        });
+
+        const testBed = getTestBed();
+        myCoordinator = testBed.get(CoordinatorService);
+    });
+
+
+    it('should be created', () => {
+        const service: CoordinatorService = TestBed.get(CoordinatorService);
+        expect(service).toBeTruthy();
+    });
+});
