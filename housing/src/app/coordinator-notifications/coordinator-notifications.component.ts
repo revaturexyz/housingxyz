9b65ff14c9f33a56daf1d57f986e01825f043a73
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-11-14 23:15:47 -0600

    corrdinator-notifications setup (pre-date handler)

--- housing/src/app/coordinator-notifications/coordinator-notifications.component.ts
+++ housing/src/app/coordinator-notifications/coordinator-notifications.component.ts
@@ -7,6 +7,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoordinatorNotificationsComponent implements OnInit {
 
+
+  public notifications: Array<any> = [
+    {
+    notificationId: 'fabfd4f2-9e08-4950-a13b-574b6b8beade',
+    providerId: 2,
+    centerId: 2,
+    date: Date,
+    companyName: 'Stark Properties, Inc'
+    },
+    {
+      notificationId: 6,
+      centerId: 0,
+      apiAddress: null,
+      centerName: 'Liv +',
+      contactNumber: '2143367788'
+    },
+    {
+      notificationId: 7,
+      centerId: 1,
+      apiAddress: null,
+      centerName: 'Sunshine Springs on the Hillside Waterfall Incoporated by the Rainbow over Dreams Lost in Code',
+      contactNumber: '2141231112'
+    },
+    {
+      notificationId: 8,
+      centerId: 3,
+      apiAddress: null,
+      centerName: 'Courtshire Yards',
+      contactNumber: '2141231112'
+    },
+    {
+      notificationId: 9,
+      centerId: 4,
+      apiAddress: null,
+      centerName: 'Liv +',
+      contactNumber: '2143367788'
+    },
+    {
+      notificationId: 1,
+      centerId: 5,
+      apiAddress: null,
+      centerName: 'Sunshine Springs',
+      contactNumber: '2141231112'
+    },
+    {
+      notificationId: 2,
+      centerId: 6,
+      apiAddress: null,
+      centerName: 'Courtshire Yards',
+      contactNumber: '2141231112'
+    },
+    {
+      notificationId: 3,
+      centerId: 0,
+      apiAddress: null,
+      centerName: 'Liv +',
+      contactNumber: '2143367788'
+    },
+    {
+      notificationId: 4,
+      centerId: 1,
+      apiAddress: null,
+      centerName: 'Sunshine Springs',
+      contactNumber: '2141231112'
+    },
+    {
+      notificationId: 5,
+      centerId: 3,
+      apiAddress: null,
+      centerName: 'Courtshire Yards',
+      contactNumber: '2141231112'
+    },
+    {
+      notificationId: 6,
+      centerId: 4,
+      apiAddress: null,
+      centerName: 'Liv +',
+      contactNumber: '2143367788'
+    },
+    {
+      notificationId: 7,
+      centerId: 5,
+      apiAddress: null,
+      centerName: 'Sunshine Springs',
+      contactNumber: '2141231112'
+    },
+    {
+      notificationId: 8,
+      centerId: 6,
+      apiAddress: null,
+      centerName: 'Courtshire Yards',
+      contactNumber: '2141231112'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
