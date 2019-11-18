ee97f198fafceffccdb828d03dcea5c00d7f3bf5
Mark Moore <mark.c.moore@hotmail.com>
2019-11-18 15:54:49 -0600

    code checked for first PR to master. PR of Complex.Lib/Models

--- complex/src/Revature.Complex.Lib/Models/Amenity.cs
+++ complex/src/Revature.Complex.Lib/Models/Amenity.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace Revature.Complex.Lib.Models
 {
-    public class Amenity
-    {
-      public int AmenityId { get; set; }
-      public string AmenityType { get; set; }
-      public string Description { get; set; }
-    }
+  public class Amenity
+  {
+    public Guid AmenityId { get; set; }
+    public string AmenityType { get; set; }
+    public string Description { get; set; }
+  }
 
 }
