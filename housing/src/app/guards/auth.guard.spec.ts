6611373163eec65805ef32b2a2b46b0b91934502
jacobdavis1 <jacob.marquez.davis@hotmail.com>
2019-11-18 12:34:34 -0600

    Fixed tests.

--- housing/src/app/guards/auth.guard.spec.ts
+++ housing/src/app/guards/auth.guard.spec.ts
@@ -1,10 +1,22 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Router} from '@angular/router';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([
+        {
+          path: '',
+          component: BlankComponent
+        }, {
+          path: 'login-splash',
+          component: BlankComponent
+        }]),
+        HttpClientTestingModule],
       providers: [AuthGuard]
     });
   });
@@ -13,3 +25,7 @@ describe('AuthGuard', () => {
     expect(guard).toBeTruthy();
   }));
 });
+
+class BlankComponent {
+
+}
--- housing/src/app/services/interceptor.service.spec.ts
+++ housing/src/app/services/interceptor.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Router} from '@angular/router';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { InterceptorService } from './interceptor.service';
 
 describe('InterceptorService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule.withRoutes([
+      {
+        path: '',
+        component: BlankComponent
+      }, {
+        path: 'login-splash',
+        component: BlankComponent
+      }]),
+      HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: InterceptorService = TestBed.get(InterceptorService);
     expect(service).toBeTruthy();
   });
 });
+
+class BlankComponent {
+
+}
