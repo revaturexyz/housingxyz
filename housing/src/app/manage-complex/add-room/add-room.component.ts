3084e3893c204d28397192d3c72b7f1261dfae79
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-11-19 13:57:21 -0600

    add-room component form layout

--- housing/src/app/manage-complex/add-room/add-room.component.ts
+++ housing/src/app/manage-complex/add-room/add-room.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'dev-add-room',
   templateUrl: './add-room.component.html',
@@ -10,6 +12,28 @@ export class AddRoomComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // roomId: number;
+  // apiAddress: Address;
+  // roomNumber: string;
+  // numberOfBeds: number;
+  // apiRoomType: RoomType;
+  // isOccupied: boolean;
+  // apiAmenity: Amenity[];
+  // startDate: Date;
+  // endDate: Date;
+  // apiComplex: Complex;
+  // gender: string;
+  // leaseLength: number;
+
+
+  postAddRoom() {
+    console.log('Add Button Pressed');
+  }
+
+  cancelAddRoom() {
+    console.log('Cancel Button Pressed');
   }
 
 }
