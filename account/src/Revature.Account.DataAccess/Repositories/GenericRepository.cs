a06b26f8ae268ed0c6458abf928c944aed551ee6
kmanrulze <kcguzman@comcast.net>
2019-11-20 10:25:08 -0600

    Removed logging from repo to reduce code redundancy

--- account/src/Revature.Account.DataAccess/Repositories/GenericRepository.cs
+++ account/src/Revature.Account.DataAccess/Repositories/GenericRepository.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Serilog;
 
 namespace Revature.Account.DataAccess.Repositories
 {
@@ -13,110 +12,50 @@ namespace Revature.Account.DataAccess.Repositories
   {
     private readonly AccountDbContext _context;
     private readonly Mapper mapper;
-    private readonly ILogger _logger;
 
-    public GenericRepository(AccountDbContext db, ILogger logger)
+    public GenericRepository(AccountDbContext db)
     {
       _context = db ?? throw new ArgumentNullException("Context cannot be null.", nameof(db));
       this.mapper = new Mapper();
-      this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
     }
 
     #region Provider
 
     public async Task<ProviderAccount> GetProviderAccountByIdAsync(Guid providerId)
     {
-      try
-      {
-        _logger.Information("Getting Provider by ID: {providerId}", providerId);
-        var provider = await _context.ProviderAccount
+      var provider = await _context.ProviderAccount
         .AsNoTracking()
         .Include(p => p.Coordinator)
         .Include(p => p.Status)
         .FirstOrDefaultAsync(p => p.ProviderId == providerId);
-        return (provider != null ? mapper.MapProvider(provider) : null);
-      }
-      catch(InvalidOperationException e)
-      {
-        _logger.Error("Invalid Operation Exception Error. Error message: " +  e.Message);
-        return null;
-      }
-      catch(Exception e)
-      {
-        _logger.Error("Something went wrong when getting a provider account by ID. Error: " + e.Message);
-        return null;
-      }
-      
+      return (provider != null ? mapper.MapProvider(provider) : null);
     }
 
     public void AddProviderAccountAsync(ProviderAccount newAccount)
     {
-      try
-      {
-        _logger.Information("Adding provider account for {name}", newAccount.Name);
-        var newEntity = mapper.MapProvider(newAccount);
-        _context.Add(newEntity);
-      }
-      catch(InvalidOperationException e)
-      {
-        _logger.Error("Invalid Operation Exception Error. Error message: " + e.Message);
-      }
-      catch(Exception e)
-      {
-        _logger.Error("Something went wrong adding a provider account. Error message: " + e.Message);
-      }
+      var newEntity = mapper.MapProvider(newAccount);
+      _context.Add(newEntity);
     }
+
     public async Task<bool> UpdateProviderAccountAsync(ProviderAccount providerAccount)
     {
-      try
-      {
-        _logger.Information("Updating providerAccount for {Name}", providerAccount.Name);
-        var existingEntity = await _context.ProviderAccount.FindAsync(providerAccount.ProviderId);
-        if (existingEntity == null)
-        {
-          return false;
-        }
-        var updatedEntity = mapper.MapProvider(providerAccount);
-        _context.Entry(existingEntity).CurrentValues.SetValues(updatedEntity);
-        return true;
-      }
-      catch (InvalidOperationException e)
-      {
-        _logger.Error("Invalid Operation Exception Error. Error message: " + e.Message);
-        throw new InvalidOperationException(e.Message);
-      }
-      catch (Exception e)
-      {
-        _logger.Error("Something went wrong updating provider account. Error message: " + e.Message);
-        throw new Exception(e.Message);
-      }
+      var existingEntity = await _context.ProviderAccount.FindAsync(providerAccount.ProviderId);
+      if (existingEntity == null)
+        return false;
+
+      var updatedEntity = mapper.MapProvider(providerAccount);
+      _context.Entry(existingEntity).CurrentValues.SetValues(updatedEntity);
+      return true;
     }
 
     public async Task<bool> DeleteProviderAccountAsync(Guid providerId)
     {
-      try
-      {
-        _logger.Information("Deleting provider account for given ID: {providerId}", providerId);
-        var entityToBeRemoved = await _context.ProviderAccount.FindAsync(providerId);
-        if (entityToBeRemoved == null)
-        {
-          return false;
-        }
-        _context.Remove(entityToBeRemoved);
-        return true;
-      }
-      catch(InvalidOperationException e)
-      {
-        _logger.Error("Invalid Operation Exception Error. Error message: " + e.Message);
-        throw new InvalidOperationException(e.Message);
-      }
-      catch(Exception e)
-      {
-        _logger.Error("Something went wrong deleting a provier account. Error message: " + e.Message);
-        throw new Exception(e.Message);
-      }
-      
+      var entityToBeRemoved = await _context.ProviderAccount.FindAsync(providerId);
+      if (entityToBeRemoved == null)
+        return false;
+
+      _context.Remove(entityToBeRemoved);
+      return true;
     }
 
     #endregion
@@ -125,22 +64,11 @@ namespace Revature.Account.DataAccess.Repositories
 
     public async Task<CoordinatorAccount> GetCoordinatorAccountByIdAsync(Guid coordinatorId)
     {
-      try
-      {
-        var coordinator = await _context.CoordinatorAccount
+      var coordinator = await _context.CoordinatorAccount
         .AsNoTracking()
         .Include(c => c.Notifications)
         .FirstOrDefaultAsync(p => p.CoordinatorId == coordinatorId);
-        return (coordinator != null ? mapper.MapCoordinator(coordinator) : null);
-      }
-      catch(InvalidOperationException e)
-      {
-
-      }
-      catch(Exception e)
-      {
-
-      }
+      return (coordinator != null ? mapper.MapCoordinator(coordinator) : null);
     }
 
     #endregion
