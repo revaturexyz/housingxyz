6a0f0429260edd903e6747bb42bf6aca2b888aa1
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-11-20 12:22:19 -0600

    edit room variables work

--- housing/src/app/manage-complex/edit-room/edit-room.component.ts
+++ housing/src/app/manage-complex/edit-room/edit-room.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { Room } from '../../../interfaces/room';
+import { RoomType } from '../../../interfaces/room-type';
+import { Gender } from '../../../interfaces/gender';
+import { Complex } from 'src/interfaces/complex';
 
 @Component({
   selector: 'dev-edit-room',
@@ -7,7 +14,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditRoomComponent implements OnInit {
 
-  constructor() { }
+  @Input() complexControl: Complex;
+  @Input() targetRoom: Room;
+
+  formRoom: Room;
+
+  constructor() {
+    this.formRoom = this.targetRoom;
+   }
 
   ngOnInit() {
   }
