3e27852d0caa7ecfc370affa5fa37721d7334a00
Colton Clary <coltonclary0806@yahoo.com>
2019-11-20 11:49:10 -0600

    Added HasCarAsync Test in Repository Tests

--- tenant/src/Revature.Tenant.Tests/ApiTests/ApiTestData.cs
+++ tenant/src/Revature.Tenant.Tests/ApiTests/ApiTestData.cs
@@ -23,7 +23,17 @@ namespace Revature.Tenant.Tests.ApiTests
         AddressId = Guid.Parse("d9e4077e-0e5e-4201-8801-a2ea40b8c0b5"),
         RoomId = Guid.Parse("7b774b35-ce79-4fa9-b860-93b4fa1ddc6a"),
         CarId = 1,
-        BatchId = 2
+        BatchId = 2,
+        Car = new Lib.Models.Car
+        {
+          Id = 1,
+          LicensePlate = "LicensePlate",
+          Make = "Make",
+          Model = "Model",
+          Color = "Color",
+          Year = "Year",
+          State = "TX"
+        },
       },
 
       new LibMod.Tenant
@@ -36,10 +46,20 @@ namespace Revature.Tenant.Tests.ApiTests
         AddressId = Guid.Parse("e2c6939b-0f22-4746-b2f4-036a1c74015e"),
         RoomId = Guid.Parse("7ec98f58-e416-4682-a3c8-457eafedc938"),
         CarId = 2,
-        BatchId = 2
+        BatchId = 2,
+        Car = new Lib.Models.Car
+        {
+          Id = 1,
+          LicensePlate = "LicensePlate",
+          Make = "Make",
+          Model = "Model",
+          Color = "Color",
+          Year = "Year",
+          State = "TX"
+        },
       },
     };
-      
+
     internal static Mock<ITenantRepository> MockTenantRepo(List<LibMod.Tenant> testTenant)
     {
       var mockRepo = new Mock<ITenantRepository>();
--- tenant/src/Revature.Tenant.Tests/DataTests/TenantRepositoryTest.cs
+++ tenant/src/Revature.Tenant.Tests/DataTests/TenantRepositoryTest.cs
@@ -1,28 +1,130 @@
 using Xunit;
 using Revature.Tenant.DataAccess.Repository;
 using Revature.Tenant.DataAccess;
+using System.Threading.Tasks;
+using System;
 
 namespace Revature.Tenant.Tests.DataTests
 {
+  /// <summary>
+  /// Unit tests for data access methods in TenantRepository class
+  /// </summary>
+  public class TenantRepositoryTests
+  {
     /// <summary>
-    /// Unit tests for data access methods in RoomRepository class
+    /// A test for the Constructor to Construct 
     /// </summary>
-    public class AddressRepositoryTests
+    [Fact]
+    public void ConstructorShouldConstruct()
     {
-      /// <summary>
-      /// Checks that the constructor can construct a new AddressRepository object
-      /// </summary>
-      [Fact]
-      public void ConstructorShouldConstruct()
+      // Arrange 
+      var options = TestDbInitializer.InitializeDbOptions("TestConstructor");
+      using var db = TestDbInitializer.CreateTestDb(options);
+      var mapper = new Mapper();
+      // Act 
+      var repo = new TenantRepository(db, mapper);
+      // Assert 
+    }
+
+    /// <summary>
+    /// Checks that AddAsync creates a new Tenant in Db
+    /// </summary>
+    [Fact]
+    public async Task AddShouldAddTest()
+    {
+      // Arrange
+      var options = TestDbInitializer.InitializeDbOptions("AddShouldAddTest");
+      using var db = TestDbInitializer.CreateTestDb(options);
+      var mapper = new Mapper();
+      var repo = new TenantRepository(db, mapper);
+      var tenant = new Lib.Models.Tenant
       {
-        // Arrange 
-        var options = TestDbInitializer.InitializeDbOptions("TestConstructor");
-        using var db = TestDbInitializer.CreateTestDb(options);
-        var mapper = new Mapper();
-        // act 
-        var repo = new TenantRepository(db, mapper);
-        // assert 
-      }
+        Id = Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67d"),
+        Email = "firstname@email.com",
+        Gender = "Male",
+        FirstName = "Clary",
+        LastName = "Colton",
+        AddressId = Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67c"),
+        RoomId = Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67c"),
+        CarId = 1,
+        BatchId = 3,
+        Car = new Lib.Models.Car
+        {
+          Id = 1,
+          LicensePlate = "LicensePlate",
+          Make = "Make",
+          Model = "Model",
+          Color = "Color",
+          Year = "Year",
+          State = "TX"
+        },
+      };
+
+      // Act
+      await repo.AddAsync(tenant);
+      await repo.SaveAsync();
+      var check = await repo.GetByIdAsync(Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67d"));
+      // Assert
+
+      Assert.NotNull(check);
+      Assert.Equal(tenant.Email, check.Email);
+      Assert.Equal(tenant.Gender, check.Gender);
+      Assert.Equal(tenant.FirstName, check.FirstName);
+      Assert.Equal(tenant.LastName, check.LastName);
+      Assert.Equal(tenant.AddressId, check.AddressId);
+      Assert.Equal(tenant.RoomId, check.RoomId);
+      Assert.Equal(tenant.CarId, check.CarId);
+      Assert.Equal(tenant.BatchId, check.BatchId);
+    }
+
+    /// <summary>
+    /// Checks that HasCarAsync Returns true if the tenant has a car
+    /// </summary>
+    [Fact]
+    public async Task HasCarShouldReturnTrueIfHasCar()
+    {
+      //Arrange
+      var options = TestDbInitializer.InitializeDbOptions("AddShouldAddTest");
+      using var db = TestDbInitializer.CreateTestDb(options);
+      var mapper = new Mapper();
+      var repo = new TenantRepository(db, mapper);
+      var tenant = new Lib.Models.Tenant
+      {
+        Id = Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67d"),
+        Email = "firstname@email.com",
+        Gender = "Male",
+        FirstName = "Clary",
+        LastName = "Colton",
+        AddressId = Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67c"),
+        RoomId = Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67c"),
+        CarId = 1,
+        BatchId = 3,
+        Car = new Lib.Models.Car
+        {
+          Id = 1,
+          LicensePlate = "LicensePlate",
+          Make = "Make",
+          Model = "Model",
+          Color = "Color",
+          Year = "Year",
+          State = "TX"
+        },
+      };
+
+
+      //Act
+
+      await repo.AddAsync(tenant);
+      await repo.SaveAsync();
+
+      var check = await repo.HasCarAsync(Guid.Parse("fa4d6c6e-9650-44c9-8c6b-5aebd3f9a67d"));
+
+      //Assert
+
+      Assert.True(check);
 
     }
+
+
+  }
 }
