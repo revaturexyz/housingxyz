0aef0002a2b37db7e429e4320190f2cf15ecfbcc
Colton Clary <coltonclary0806@yahoo.com>
2019-11-19 18:23:31 -0600

    Added Data Tests for Batches and Tenants

---
+++ tenant/src/Revature.Tenant.Tests/DataTests/BatchesTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Revature.Tenant.Tests.ApiTests;
+using Revature.Tenant.Api.Models;
+using DatMod = Revature.Tenant.DataAccess.Entities;
+
+namespace Revature.Tenant.Tests.DataTests
+{
+  public class BatchesTest
+  {
+    [Fact]
+    public void ConstructorShouldConstructWithSetters()
+    {
+      //Arrange
+      var batchId = 1;
+      var batchLanguage = "C#";
+      DateTime startDate = new DateTime(2019, 1, 5);
+      DateTime endDate = new DateTime(2019, 1, 5);
+
+      //Act
+      var apiBatch = new DatMod.Batches()
+      {
+        Id = batchId,
+        BatchLanguage = batchLanguage,
+        StartDate = startDate,
+        EndDate = endDate
+      };
+
+      //Assert 
+      Assert.Equal(batchId, apiBatch.Id);
+      Assert.Equal(batchLanguage, apiBatch.BatchLanguage);
+      Assert.Equal(startDate, apiBatch.StartDate);
+      Assert.Equal(endDate, apiBatch.EndDate);
+    }
+  }
+}
