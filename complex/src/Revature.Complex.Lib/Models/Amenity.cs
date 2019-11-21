38112b39b092b993e2ae2ac6df3d94f97ef183d5
Mark Moore <mark.c.moore@hotmail.com>
2019-11-21 16:39:50 -0600

    Changes implemented for PR

--- complex/src/Revature.Complex.Lib/Models/Amenity.cs
+++ complex/src/Revature.Complex.Lib/Models/Amenity.cs
@@ -1,18 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Revature.Complex.Lib.Models
 {
   /// <summary>
-  /// This object defines an amenity. It has the amenity name and GUID (id).
-  /// Some amenities are scoped to a particular room, while others apply to a whole complex.
+  /// Logic Amenity Model. Use it as parameter to pass into/received from Repository
   /// </summary>
   public class Amenity
-    {
-      public Guid AmenityId { get; set; }
-      public string AmenityType { get; set; }
-      public string Description { get; set; }
-    }
+  {
+    public Guid AmenityId { get; set; }
+    public string AmenityType { get; set; }
+    public string Description { get; set; }
+  }
 
 }
