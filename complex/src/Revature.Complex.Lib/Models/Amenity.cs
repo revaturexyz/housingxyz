dbec6934a241ac2c356bbd9bdecfc67da3e84bc1
Mark Moore <mark.c.moore@hotmail.com>
2019-11-21 16:37:18 -0600

    Changes implemented for PR

--- complex/src/Revature.Complex.Lib/Models/Amenity.cs
+++ complex/src/Revature.Complex.Lib/Models/Amenity.cs
@@ -6,10 +6,10 @@ namespace Revature.Complex.Lib.Models
   /// Logic Amenity Model. Use it as parameter to pass into/received from Repository
   /// </summary>
   public class Amenity
-    {
-      public Guid AmenityId { get; set; }
-      public string AmenityType { get; set; }
-      public string Description { get; set; }
-    }
+  {
+    public Guid AmenityId { get; set; }
+    public string AmenityType { get; set; }
+    public string Description { get; set; }
+  }
 
 }
