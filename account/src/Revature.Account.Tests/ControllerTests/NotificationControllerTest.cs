d76b36b4b2a881e2dd8d627695400fc818b1bc4f
PhatNguyen203 <Lee0ne203@gmail.com>
2019-11-21 14:36:16 -0600

    finished controller tests reorganized the code removed space

--- account/src/Revature.Account.Tests/ControllerTests/NotificationControllerTest.cs
+++ account/src/Revature.Account.Tests/ControllerTests/NotificationControllerTest.cs
@@ -7,6 +7,7 @@ using Xunit;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Revature.Account.Lib.Model;
 
 namespace Revature.Account.Tests.ControllerTests
 {
@@ -24,6 +25,73 @@ namespace Revature.Account.Tests.ControllerTests
         .Returns(Task.Run(() => helper.Notifications.Where(n => n.NotificationId == notificationId).ToList()));
 
       Assert.NotNull(await helper.NotificationController.GetNotificationByCoordinatorIdAsync(coordinatorId) as OkObjectResult);
+    }
+
+    [Fact]
+    public async Task CreateNewNotificationSuccessfullyVerifyTestAsync()
+    {
+      TestHelper helper = new TestHelper();
+      Guid coordinatorId = helper.Coordinators[0].CoordinatorId;
+      Guid providerId = helper.Providers[0].ProviderId;
+      var newNotification = new Notification();
+      newNotification.ProviderId = providerId;
+      newNotification.CoordinatorId = coordinatorId; 
+      Guid newNotificationId = newNotification.NotificationId;
+
+      helper.Repository
+        .Setup(x => x.AddNotification(It.IsAny<Notification>()))
+        .Verifiable();
+
+      var newNofi = await helper.NotificationController.Post(newNotification);
+
+      helper.Repository
+          .Verify();
+    }
+
+    [Fact]
+    public async Task UpdateNotificationAsyncSuccessfulVerificationAsync()
+    {
+      TestHelper helper = new TestHelper();
+      Guid notificationId = helper.Notifications[0].NotificationId;
+      Guid coordinatorId = helper.Notifications[0].CoordinatorId;
+      Guid providerId = helper.Notifications[0].ProviderId;
+      var updatedNotification = helper.Notifications[0];
+      updatedNotification.Status.StatusText = "Under Review";
+
+      helper.Repository
+          .Setup(x => x.GetNotificationByIdAsync(It.IsAny<Guid>()))
+              .Returns(Task.FromResult(helper.Notifications[0]))
+              .Verifiable();
+      helper.Repository
+          .Setup(x => x.UpdateNotificationAsync(It.IsAny<Notification>()))
+              .Returns(Task.FromResult(true))
+              .Verifiable();
+
+      var updatedResult = await helper.NotificationController.Patch(coordinatorId, updatedNotification);
+
+      helper.Repository
+          .Verify();
+    }
+
+    [Fact]
+    public async Task DeleteNotificationAsyncSuccessfulVerificationAsync()
+    {
+      TestHelper helper = new TestHelper();
+      Guid notificationId = helper.Notifications[0].NotificationId;
+
+      helper.Repository
+          .Setup(x => x.GetNotificationByIdAsync(It.IsAny<Guid>()))
+              .Returns(Task.FromResult(helper.Notifications[0]))
+              .Verifiable();
+      helper.Repository
+          .Setup(x => x.DeleteNotificationByIdAsync(It.IsAny<Guid>()))
+              .Returns(Task.FromResult(true))
+              .Verifiable();
+
+      var deleted = await helper.NotificationController.Delete(notificationId);
+
+      helper.Repository
+          .Verify();
     }
   }
 }
