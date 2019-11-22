4b69a8829e696107f2ab7cf4aa9b458f0d24a683
Wesley Tang <wesley.tang25@gmail.com>
2019-11-22 16:25:28 -0600

    updated environment for docker localhost

--- housing/src/environments/environment.ts
+++ housing/src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
     // provider: 'http://192.168.99.100:10080/'
     provider: 'http://localhost:10080/',
     // provider: 'http://localhost:5000/'
-    coordinator: 'http://localhost:4200'
+    coordinator: '192.168.99.100:10080/'
   }
 };
 
