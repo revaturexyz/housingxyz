3658731d08fb27f384dfc8d7a7251aeda582b52b
Wesley Tang <wesley.tang25@gmail.com>
2019-11-25 16:52:59 -0600

    fixed Angular Testing errors for select-tenant and search-tenant still have 3 errors: 2 formGroup not form and 1 addTenant being empty

--- housing/src/app/select-tenant/select-tenant.component.spec.ts
+++ housing/src/app/select-tenant/select-tenant.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectTenantComponent } from './select-tenant.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SelectTenantComponent', () => {
   let component: SelectTenantComponent;
   let fixture: ComponentFixture<SelectTenantComponent>;
+  const selectTenSpy = jasmine.createSpyObj('SelectTenantComponent', ['deleteGo'])
+  const activeRoute = jasmine.createSpyObj('ActivatedRoute', ['id'])
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SelectTenantComponent ]
+      declarations: [ SelectTenantComponent ],
+      imports: [ RouterTestingModule, HttpClientTestingModule ]
     })
     .compileComponents();
   }));
--- housing/src/app/services/tenant-searcher.service.spec.ts
+++ housing/src/app/services/tenant-searcher.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TenantSearcherService } from './tenant-searcher.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('TenantSearcherService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
+    providers: [
+      TenantSearcherService
+    ]
+  }));
 
   it('should be created', () => {
     const service: TenantSearcherService = TestBed.get(TenantSearcherService);
