5e31aac19062135b0bca6d940c0f013ad7b99405
kmanrulze <kcguzman@comcast.net>
2019-11-25 12:32:10 -0600

    Added methods and const to status class

--- account/src/Revature.Account.Lib/Model/Status.cs
+++ account/src/Revature.Account.Lib/Model/Status.cs
@@ -14,5 +14,33 @@ namespace Revature.Account.Lib.Model
   {
     public int StatusId { get; set; }
     public string StatusText { get; set; }
+
+    private const string pendingNotification = "Pending";
+    private const string acceptedNotification = "Accepted";
+    private const string rejectedNotification = "Rejected";
+    private const string underReviewNotification = "Under Review";
+
+    public void changeStatus(int statusId)
+    {
+      switch (statusId)
+      {
+        case 1:
+          StatusText = pendingNotification;
+          break;
+        case 2:
+          StatusText = acceptedNotification;
+          break;
+        case 3:
+          StatusText = rejectedNotification;
+          break;
+        case 4:
+          StatusText = underReviewNotification;
+          break;
+      }
+    }
+    public string getStatus()
+    {
+      return StatusText;
+    }
   }
 }
