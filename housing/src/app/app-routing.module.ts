10d2781c1fe0c346df18a5e576ccab99a29f5cef
yetinight <gang@ucsd.edu>
2019-11-26 15:23:56 -0600

    got template to display

--- housing/src/app/app-routing.module.ts
+++ housing/src/app/app-routing.module.ts
@@ -9,14 +9,18 @@ import { AddProviderComponent } from './add-provider/add-provider.component';
 import { CoordinatorNotificationsComponent } from './coordinator-notifications/coordinator-notifications.component';
 import { NotificationDetailsComponent } from './coordinator-notifications/notification-details/notification-details.component';
 import { AuthGuard } from './guards/auth.guard';
+import { AssignTenantToRoomComponent } from './assign-tenant-to-room/assign-tenant-to-room.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent},
   { path: 'show-rooms', component: UpdateRoomComponent },
-  { path: 'provider-select', component: ProviderSelectComponent, canActivate: [AuthGuard] },
+  { path: 'provider-select', component: ProviderSelectComponent},
   { path: 'add-provider', component: AddProviderComponent },
   { path: 'coordinator-notifications', component: CoordinatorNotificationsComponent },
   { path: 'coordinator-notifications/:id', component: NotificationDetailsComponent },
+  // dev-assign-tenant-to-room
+  { path: 'dev-assign-tenant-to-room', component: AssignTenantToRoomComponent },
+
   // { path: "location-rooms/:id", component: LocationRoomsComponent }
   { path: 'add-complex', component: AddComplexComponent },
   { path: 'addroom', component: AddRoomComponent }
