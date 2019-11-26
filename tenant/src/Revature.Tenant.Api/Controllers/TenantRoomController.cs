4c4b0645e0f04bac46fc43ebd8a712d79c80bbcd
James <oneinfinity@yahoo.com>
2019-11-26 15:55:19 -0600

    created prototype for getting list of avail rooms from room service and returning to coordinator

--- tenant/src/Revature.Tenant.Api/Controllers/TenantRoomController.cs
+++ tenant/src/Revature.Tenant.Api/Controllers/TenantRoomController.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Revature.Tenant.Lib.Interface;
+using Revature.Tenant.Lib.Models;
 
 namespace Revature.Tenant.Api.Controllers
 {
@@ -25,21 +28,49 @@ namespace Revature.Tenant.Api.Controllers
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetTenantsByRoomId([FromQuery] Guid roomId)
+    public async Task<IActionResult> GetTenantsByRoomId([FromQuery] string gender, [FromQuery] DateTime endDate)
     {
       try
       {
-        _logger.LogInformation("Getting Tenants by Room Id...");
+        using (var client = new HttpClient())
+        {
+          string baseUri = "baseUriForRoomService.com/";
+          string resourceUri = "api/rooms?gender=" + gender + "&endDate=" + endDate;
+          var response = await client.GetAsync(baseUri+resourceUri);
+          if (response.IsSuccessStatusCode)
+          {
+            var contentAsString = await response.Content.ReadAsStringAsync();
+            var availableRooms = JsonSerializer.Deserialize<List<Tuple<Guid,int>>>(contentAsString);
 
-        IEnumerable<Lib.Models.Tenant> tenants = await _repository.GetTenantsByRoomId(roomId);
+            var roomsWithTenants = new List<RoomInfo>();
 
-        _logger.LogInformation("Success.");
+            _logger.LogInformation("Getting Tenants by Room Id...");
 
-        return Ok(tenants);
+            foreach (var room in availableRooms)
+            {
+              roomsWithTenants.Add(
+                new RoomInfo
+                {
+                  RoomId = room.Item1,
+                  NumberOfBeds = room.Item2,
+                  Tenants = await _repository.GetTenantsByRoomId(room.Item1) as List<Lib.Models.Tenant>
+                }
+              );
+            }
+
+            _logger.LogInformation("Success.");
+
+
+            return Ok(roomsWithTenants);
+          } else
+          {
+            return BadRequest();
+          }
+        }
       }
-      catch(KeyNotFoundException ex)
+      catch (ArgumentNullException ex)
       {
-        _logger.LogError(ex.Message);
+        _logger.LogError(ex, "Tried to parse a null tenant(?)");
 
         return NotFound(ex);
       }
