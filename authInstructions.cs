b9cf75ddefb91f30f0f618700269e4899f394a9a
jacobdavis1 <jacob.marquez.davis@hotmail.com>
2019-11-30 17:36:12 -0600

    Fixed appsettings, added instructions to copy barebones auth stuff

---
+++ authInstructions.cs
@@ -0,0 +1,38 @@
+// Instructions to add auth and policies to a project
+
+// 1. Add the following to ConfigureServices
+services.AddSingleton<IAuthorizationHandler, RoleRequirementHandler>();
+
+services.AddAuthentication(options =>
+{
+options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+options.Authority = $"http://{Configuration.GetSection("Auth0").GetValue<string>("Domain")}/";
+options.Audience = Configuration.GetSection("Auth0").GetValue<string>("Audience");
+});
+
+services.AddAuthorization(options => {
+options.AddPolicy("ApprovedProviderRole", policy =>
+    policy.Requirements.Add(new RoleRequirement("approved_provider")));
+options.AddPolicy("CoordinatorRole", policy =>
+    policy.Requirements.Add(new RoleRequirement("coordinator")));
+
+// To fix needing to manually specify the schema every time I want to call [Authorize]
+// Found it at https://github.com/aspnet/AspNetCore/issues/2193
+options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+    .RequireAuthenticatedUser()
+    .Build();
+});
+
+// 2. Add the following to Configure
+app.UseAuthorization();
+
+// 3. Copy the following files into your .Api project:
+//  a. RoleRequirement
+//  b. RoleRequirementHandler
+//
+// Verify the namespace and everything is all good for your project
+
+// 4. Get the Auth0 section from Slack and copy it into your appsettings.Development.json
