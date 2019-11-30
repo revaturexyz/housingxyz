99e9f76008add893867befe2434cf48d107c0e9f
mariellenolasco <mariellenolasco@gmail.com>
2019-11-30 17:33:48 -0600

    Added more tests

--- tenant/src/Revature.Tenant.Api/ServiceBus/IRoomService.cs
+++ tenant/src/Revature.Tenant.Api/ServiceBus/IRoomService.cs
@@ -1,8 +1,9 @@
 using Revature.Tenant.Lib.Models;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace Revature.Tenant.Api.ServiceBus
 {
   /// <summary>
--- tenant/src/Revature.Tenant.Tests/ApiTests/ControllerTests/TenantRoomControllerTests.cs
+++ tenant/src/Revature.Tenant.Tests/ApiTests/ControllerTests/TenantRoomControllerTests.cs
@@ -5,6 +5,7 @@ using Revature.Tenant.Api.Controllers;
 using Revature.Tenant.Api.ServiceBus;
 using Revature.Tenant.Lib.Interface;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -72,6 +73,39 @@ namespace Revature.Tenant.Tests.ApiTests.ControllerTests
       var result = await _controller.AssignTenantToRoom(Guid.NewGuid(), Guid.NewGuid());
 
       Assert.IsAssignableFrom<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task GetTenantsByRoomIdShouldSucceedAsync()
+    {
+      //Arrange (Create mock DB and Test Data.)
+      var mockRepo = new Mock<ITenantRoomRepository>();
+      var mockRepo2 = new Mock<ITenantRepository>();
+      var mockLogger = new Mock<ILogger<TenantRoomController>>();
+      var mockRoomService = new Mock<IRoomService>();
+      var _controller = new TenantRoomController(mockRepo.Object, mockRepo2.Object, mockLogger.Object, mockRoomService.Object);
+
+      mockRoomService.Setup(r => r.GetVacantRoomsAsync(It.IsAny<string>(), It.IsAny<DateTime>())).ReturnsAsync(new List<Lib.Models.AvailRoom>());
+
+      var result = await _controller.GetTenantsByRoomId("", DateTime.Now);
+
+      Assert.IsAssignableFrom<OkObjectResult>(result);
+    }
+    [Fact]
+    public async Task GetTenantsShouldReturnBadRequestAsync()
+    {
+      //Arrange (Create mock DB and Test Data.)
+      var mockRepo = new Mock<ITenantRoomRepository>();
+      var mockRepo2 = new Mock<ITenantRepository>();
+      var mockLogger = new Mock<ILogger<TenantRoomController>>();
+      var mockRoomService = new Mock<IRoomService>();
+      var _controller = new TenantRoomController(mockRepo.Object, mockRepo2.Object, mockLogger.Object, mockRoomService.Object);
+
+      mockRoomService.Setup(r => r.GetVacantRoomsAsync(It.IsAny<string>(), It.IsAny<DateTime>())).ThrowsAsync(new HttpRequestException());
+
+      var result = await _controller.GetTenantsByRoomId("", DateTime.Now);
+
+      Assert.IsAssignableFrom<BadRequestResult>(result);
     }
   }
 }
