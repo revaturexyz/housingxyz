94a6d06ec22fc150097f6bdcb1877b254f1ec579
mariellenolasco <mariellenolasco@gmail.com>
2019-11-30 17:56:30 -0600

    Addressing code smells

--- tenant/src/Revature.Tenant.Api/ServiceBus/IRoomService.cs
+++ tenant/src/Revature.Tenant.Api/ServiceBus/IRoomService.cs
@@ -1,7 +1,6 @@
 using Revature.Tenant.Lib.Models;
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Revature.Tenant.Api.ServiceBus
