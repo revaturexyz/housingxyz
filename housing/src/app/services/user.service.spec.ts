4e39060443dddb12c5e3650db1bcff285b81ed46
jacobdavis1 <jacob.marquez.davis@hotmail.com>
2019-11-30 18:09:38 -0600

    Fixed tests.

--- housing/src/app/services/user.service.spec.ts
+++ housing/src/app/services/user.service.spec.ts
@@ -1,9 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
+import { RouterTestingModule } from '@angular/router/testing';
+
+class BlankComponent {
+
+}
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule.withRoutes([
+      {
+        path: '',
+        component: BlankComponent
+      }, {
+        path: 'login-splash',
+        component: BlankComponent
+      }]),
+      HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
