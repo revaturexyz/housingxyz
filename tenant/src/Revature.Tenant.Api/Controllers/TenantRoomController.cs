ae7dd05f87f9c8e5a0e4ee80fa9a16c6bc9baff7
mariellenolasco <mariellenolasco@gmail.com>
2019-12-01 20:39:41 -0600

    Fixed update method to assign tenant to room

--- tenant/src/Revature.Tenant.Api/Controllers/TenantRoomController.cs
+++ tenant/src/Revature.Tenant.Api/Controllers/TenantRoomController.cs
@@ -121,7 +121,8 @@ namespace Revature.Tenant.Api.Controllers
         _logger.LogInformation("Assigning tenant to room");
         var tenant = await _repo2.GetByIdAsync(tenantId);
         tenant.RoomId = roomId;
-
+        _repo2.Put(tenant);
+        await _repo2.SaveAsync();
         var roomMessage = new RoomMessage()
         {
           Gender = tenant.Gender,
