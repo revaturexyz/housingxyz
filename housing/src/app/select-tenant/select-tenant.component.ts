ff314cee5c20d013d70d42a4b43b8b4233de5701
Gregory Y Favrot <gpyofavv@gmail.com>
2019-12-02 15:48:46 -0600

    Add line that was accidentally deleted

--- housing/src/app/select-tenant/select-tenant.component.ts
+++ housing/src/app/select-tenant/select-tenant.component.ts
@@ -49,7 +49,11 @@ export class SelectTenantComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
 
     this.service.selectTenant(id)
-      .then(x => this.tenant = x);
+      .then(x => 
+        {
+          this.tenant = x;
+          this.tenantLoaded = true;
+        });
   }
 
 }
