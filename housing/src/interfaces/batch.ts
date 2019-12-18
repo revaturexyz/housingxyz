f396d66531c0abe0429ab4c8c7cd86acd55fe77e
mariellenolasco <mariellenolasco@gmail.com>
2019-12-18 17:31:04 -0600

    fixed conflicts

--- housing/src/interfaces/batch.ts
+++ housing/src/interfaces/batch.ts
@@ -1,15 +1,7 @@
 export interface Batch {
-<<<<<<< HEAD
     batchId: number;
     batchCurriculum: string;
     startDate: Date;
     endDate: Date;
     trainingCenter: string;
-=======
-  batchId: number;
-  batchCurriculum: string;
-  startDate: Date;
-  endDate: Date;
-  trainingCenter: string;
->>>>>>> 260cedb78c31a9037807dd7d3efa198dc97b33ed
 }
